1e69746faceae6095c4f78ed6c52f7e27a3d09dd
dongjunsong <papi64129955@gmail.com>
2024-07-10 20:00:11 +0900

    create main page & setting webcam

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,10 +1,12 @@
+import { Link } from 'react-router-dom';
 
-function MainPage(){
-    return(
-        <>
-        <h1>메인페이지</h1>
-        </>
-    );
+function MainPage() {
+  return (
+    <div>
+      <h1>메인 페이지</h1>
+      <Link to="/hand-detection">Hand Detection으로 이동</Link>
+    </div>
+  );
 }
 
 export default MainPage;
