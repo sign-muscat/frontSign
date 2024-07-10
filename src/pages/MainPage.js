477dd6362b458701897b2228a9ac77f4c21d3810
ppyye <154950167+ppyye@users.noreply.github.com>
2024-07-10 20:37:19 +0900

    #2 프론트 폰트 및 레이아웃 수정 (#3) (#7)
    * setting: front react 환경 셋팅
    
    * refact: default font change
    
    * refact: update default layout
    
    ---------
    
    Co-authored-by: ddalla0425 <hiwgod92@gmail.com>

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,8 +1,10 @@
+import {Box, Heading} from "@chakra-ui/react";
 
 function MainPage(){
     return(
         <>
-        <h1>메인페이지</h1>
+        <Box>메인페이지</Box>
+            <Heading>안녕하세요 저는 폰트입니다. 잘 적용 되었나요??</Heading>
         </>
     );
 }
