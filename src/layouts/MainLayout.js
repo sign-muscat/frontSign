34cd7deead4066fdb9bb7760a439cbfe65c4b81d
ppyye <6076hs@gmail.com>
2024-07-10 13:16:57 +0900

    refact: update default layout

--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -1,10 +1,18 @@
+import { Container, Divider } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
+import Header from "../components/Header";
 
 function MainLayout() {
 
     return (
         <main>
-            <Outlet/>
+            <Container maxW='2xl' border='1px solid'>
+                
+                <Header/>
+                <Divider mb={5}/>
+                <Outlet/>
+
+            </Container>
         </main>
     );
 }
