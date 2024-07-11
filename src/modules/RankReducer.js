a6814bd716ef711cb21148a66784762e17b51f84
ppyye <6076hs@gmail.com>
2024-07-11 13:43:49 +0900

    feat: create Rank Reducer

---
+++ src/modules/RankReducer.js
@@ -0,0 +1,21 @@
+import {createActions, handleActions} from "redux-actions";
+
+/* 초기값 */
+const initialState = {};
+
+/* 액션 */
+const GET_RANKS = 'rank/GET_RANKS';
+const SUCCESS = 'rank/SUCCESS';
+
+export const { rank : {getRanks, success}} = createActions({
+    [GET_RANKS] : result => ({ ranks : result.data }),
+    [SUCCESS] : () => ({success : true})
+});
+
+/* 리듀서 */
+const RankReducer = handleActions({
+    [GET_RANKS] : (state, {payload}) => payload,
+    [SUCCESS] : (state, {payload}) => payload
+}, initialState);
+
+export default RankReducer;
